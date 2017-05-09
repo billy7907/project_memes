e11b499da97f4fdd09a6dacda0ab2099e7cefdde
Billy Hung <billy7907@gmail.com>
2017-05-09 14:41:59 -0700

    added user validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+
+  has_secure_password
+
+  has_many :posts, dependent: :nullify
+
+  before_validation :downcase_email
+
+  VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: VALID_EMAIL
+
+  def full_name
+    "#{first_name} #{last_name}".strip.titleize
+  end
+
+  private
+
+  def downcase_email
+    self.email&.downcase!
+  end
 end
