4c389834a83cbb3427b72345f3042640b25c3c24
Billy Hung <billy7907@gmail.com>
2017-05-11 14:21:31 -0700

    added association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 
   has_many :comments, {dependent: :destroy}
+  belongs_to :users
 
   validates :title, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170509213702) do
+ActiveRecord::Schema.define(version: 20170511211928) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20170509213702) do
     t.string   "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
 
   create_table "users", force: :cascade do |t|
@@ -40,4 +42,5 @@ ActiveRecord::Schema.define(version: 20170509213702) do
   end
 
   add_foreign_key "comments", "posts"
+  add_foreign_key "posts", "users"
 end
