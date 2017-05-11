3dcb6a49692830f02af120b1fb3e4bb024b0e6c2
Billy Hung <billy7907@gmail.com>
2017-05-11 14:17:55 -0700

    added post actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,27 @@
 class PostsController < ApplicationController
 
   def index
+    @post = Post.order(created_at: :desc)
   end
-  
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new post_params
+    @post.user = current_user
+    if @post.save
+      redirect_to posts_path(@post)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def post_params
+    post_params = params.require(:post).permit(:title)
+  end
+
 end
