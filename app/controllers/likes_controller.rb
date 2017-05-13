7e0dcd8fda36c511e61aeae2ae444c230771e721
Billy Hung <billy7907@gmail.com>
2017-05-13 13:44:25 -0700

    added like action

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,22 @@
 class LikesController < ApplicationController
+
+  def create
+    like = Like.new
+    post ||= Post.find params[:post_id]
+    like.post = post
+    like.user = current_user
+    if like.save
+      redirect_to post_path(post)
+    else
+      redirect_to post_path(post)
+    end
+  end
+
+  def destroy
+    post ||= Post.find params[:post_id]
+
+    like = current_user.likes.find params[:id]
+    like.destroy
+    redirect_to post_path(post)
+  end
 end
