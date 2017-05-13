8f7a8d52cd43f9f39b2e6866aced2848b6874368
Billy Hung <billy7907@gmail.com>
2017-05-13 14:02:48 -0700

    fixed bugs

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :likes, only: [:create, :destroy]
   end
 
-  resources :users, only: [:new] do
+  resources :users, except: [:destroy] do
     resources :likes, only: :index
     delete :destroy, on: :collection
   end
