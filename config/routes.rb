a0d2791ab2fd358df3e68d35b7e1eb7c6c3dcf3c
Billy Hung <billy7907@gmail.com>
2017-05-13 13:38:19 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
   resources :posts, shallow: true,  only: [:index, :new] do
     resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
   end
 
   resources :users, only: [:new] do
+    resources :likes, only: :index
     delete :destroy, on: :collection
   end
 
