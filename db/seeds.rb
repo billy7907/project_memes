34ccdc5dfd3dfb989b3e31c891d5b52f39b4f599
Billy Hung <billy7907@gmail.com>
2017-05-15 13:47:24 -0700

    remove seed file

--- db/seeds.rb
+++
@@ -1,2 +0,0 @@
-Post.create(title: 'Charlie Scene of Hollywood Undead...', image: 'https://img-9gag-fun.9cache.com/photo/arb0dp7_460s_v1.jpg')
-Post.create(title: 'Life hack', image: 'https://s-media-cache-ak0.pinimg.com/736x/01/0b/68/010b68214bf1eeb91060732aa58bed1e.jpg')
