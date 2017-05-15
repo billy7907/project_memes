8600cf31a7d6161314344164ec6b35650cb6fdee
Billy Hung <billy7907@gmail.com>
2017-05-15 13:13:30 -0700

    fixed error

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   root 'posts#index'
 
-  resources :posts, shallow: true,  only: [:index, :new, :create] do
+  resources :posts, shallow: true do
     resources :comments, only: [:create, :destroy]
     resources :likes, only: [:create, :destroy]
   end
