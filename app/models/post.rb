d3fe7967e97edc1f586f74e8ac2c07ee3c8afabc
Billy Hung <billy7907@gmail.com>
2017-05-16 14:22:13 -0700

    removed like association

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,8 +3,6 @@ class Post < ApplicationRecord
   mount_uploader :image, ImageUploader
 
   has_many :comments, {dependent: :destroy}
-  has_many :likes, dependent: :destroy
-  has_many :likers, through: :likes, source: :user
 
   belongs_to :user
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,6 @@ class User < ApplicationRecord
   has_secure_password
 
   has_many :posts, dependent: :nullify
-  has_many :likes, dependent: :destroy
-  has_many :liked_posts, through: :likes, source: :post
 
   before_validation :downcase_email
 
