12ae167e9df3b6255382988fb90d851cdb0d8b92
Billy Hung <billy7907@gmail.com>
2017-05-21 13:25:48 -0700

    added comment user association

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
   has_many :posts, dependent: :nullify
   has_many :votes, dependent: :destroy
   has_many :voted_posts, through: :votes, source: :post
+  has_many :comments, dependent: :destroy
 
   before_validation :downcase_email
 
