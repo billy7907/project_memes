f333333d8799c7c96f1a19e6a84542c484aae072
Billy Hung <billy7907@gmail.com>
2017-05-23 13:10:06 -0700

    changed ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,21 +4,10 @@ class Ability
   def initialize(user)
 
     user ||= User.new
-    # if user.admin?
-    #   can :manage, :all
-    # end
 
     can :manage, Post do |post|
       post.user == user
     end
-
-    # can :vote, Post do |post|
-    #   user != post.user
-    # end
-    #
-    # cannot :vote, Post do |post|
-    #   user == post.user
-    # end
 
     can :manage, Vote do |v|
       user == v.user
