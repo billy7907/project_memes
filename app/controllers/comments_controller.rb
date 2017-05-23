5085e97bfe7e09c91c68ddb4e80807e2476268c8
Billy Hung <billy7907@gmail.com>
2017-05-23 13:12:27 -0700

    added comment create action

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,7 +5,16 @@ class CommentsController < ApplicationController
   end
 
   def create
-
+    comment_params = params.require(:comment).permit(:body)
+    @comment = Comment.new comment_params
+    @comment.user = current_user
+    @post = Post.find params[:post_id]
+    @comment.post = @post
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render 'posts/show'
+    end
   end
 
   def destroy
