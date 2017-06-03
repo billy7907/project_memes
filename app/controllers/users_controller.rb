469dc36d6a8f84e1bcc7206016ee4b47cde93c09
Billy Hung <billy7907@gmail.com>
2017-06-03 14:21:50 -0700

    added edit user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :load_user, except: [:new, :create]
+
   def new
     @user = User.new
   end
@@ -14,5 +16,50 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
+  def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path, notice: 'User updated!'
+    else
+      render :edit
+    end
+  end
+
+  def edit_password
+    if @user && @user.authenticate(params[:user][:current_password])
+      if params[:user][:current_password] != params[:user][:new_password]
+        if params[:user][:new_password] == params[:user][:new_password_confirmation]
+          @user.password = params[:user][:new_password]
+          @user.save
+          redirect_to root_path, notice: "Password Updated!"
+        else
+          flash[:alert] = "new password must match the new password confirmation"
+          render :edit
+        end
+      else
+        flash[:alert] = "New password must be different from the current password"
+      end
+    else
+      flash[:alert] = "Wrong password"
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def load_user
+    user = User.find_by_id params[:id]
+    if current_user.id == user.id
+      @user = user
+    end
+    @user
+  end
+
 end
